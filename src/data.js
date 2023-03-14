f68098c50be5be0865372867a592e6a682ac47a6
Thuvarakan-97 <p.thuvarakan1997@gmail.com>
2023-03-14 10:14:55 +0530

    first comment

---
+++ src/data.js
@@ -0,0 +1,116 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Google Pixe7",
+    img: "img/product-1.png",
+    price: 280000,
+    company: "GOOGLE",
+    info:
+      "The Google Pixel 7 sticks with what made the Pixel 6 a success, down to design, camera setup, and processor choice. The refinements across the board make it a worthy upgrade, though, and a continuation of Google's excellent in the smartphone department. This is the phone to get when you want Google's vision for Android.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S23",
+    img: "img/product-2.png",
+    price: 430000,
+    company: "SAMSUNG",
+    info:
+      "Samsung clearly wants the Galaxy S23 Ultra to be the ultimate camera for cinematographers and photographers. I'm certainly not a professional filmmaker, nor do I consider myself to be a shutterbug. But after using the Galaxy S23 Ultra for several days, it's clear the new camera upholds the areas in which Samsung already excels -- like low-light photography -- and pushes those strengths a little further. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "iphone 14 Pro Max",
+    img: "img/product-3.png",
+    price: 520000,
+    company: "Apple",
+    info:
+      "The iPhone 14 Pro Max features the new Apple A16 SoC with a new Display Engine to support its always-on display. The notch was also replaced by a hole-punched display which is part of a new feature called Dynamic Island. It also features a larger new 48 megapixel main camera and brighter redesigned flash. The new Photonic Engine also improves low-light performance. Like other iPhone 14 models, it adds Emergency SOS via satellite for areas where ground-based wireless services may not be available.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Samsung Galaxy M14",
+    img: "img/product-4.png",
+    price: 85000,
+    company: "Samsung",
+    info:
+      "Samsung Galaxy M14 offers the latest operating system of Android 13, One UI 5, It has Exynos 1330 (5nm), It presents a powerful CPU, It has  Octa-core (2×2.4 GHz Cortex-A78 & 6×2.0 GHz Cortex-A55), It has Mali-G68 MP2, It gives you superb performance without any lags, It presents decent apps & gaming performance.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "OnePlus 11 5G",
+    img: "img/product-5.png",
+    price: 230000,
+    company: "oneplus",
+    info:
+      "There’s a lot to like about the OnePlus 11 5G, but perhaps our favorite element is how the phone looks. The design, a flowing mix of glass and metal, really stands out. OnePlus offers the phone in Eternal Green or Titan Black. We received a green model for review, but they both look sharp. Our only gripe with the design is how slippery the phone is. The smooth glass back slides around in your hand and across any uneven surface, so you’ll definitely want to put the handset in a case for protection.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "OnePlus 11 pro 5G",
+    img: "img/product-6.png",
+    price: 280000,
+    company: "apple",
+    info:
+      "The OnePlus 11 5G flagship combines power with effortless elegance. Driven by the most extreme hardware in OnePlus history, dial every possibility up to 11. The OnePlus 11 5G is the Shape of Power. ",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Xiaomi 13 Pro",
+    img: "img/product-7.png",
+    price: 350000,
+    company: "Xiaomi",
+    info:
+      "Xiaomi 13 Pro is the latest realization of our imaging philosophy and the masterpiece behind the masterpiece. Professional optics, emotive imaging aesthetics and full Leica experience are embodied in the device to bridge the century old imaging legends and the masterpieces you are creating today.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Google Pixel 6",
+    img: "img/product-8.png",
+    price: 210000,
+    company: "Google",
+    info:
+      "Google's Pixel 6 is a sophisticated midrange phone that replaces the good, but uninspired Pixel 5. It takes a spot between the company's high-end flagship, the Pixel 6 Pro ($899), and the more affordable Pixel 5a With 5G ($449), and offers quite an impressive feature-to-value ratio for $599. With its combination of strong camera performance, long battery life, and unbeatable Android software, the Pixel 6 easily bests the competition and replaces the Pixel 5a as our Editors' Choice winner for midrange phones.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixe 7",
+  img: "img/product-1.png",
+  price: 280000,
+  company: "google",
+  info:
+    "The Google Pixel 7 sticks with what made the Pixel 6 a success, down to design, camera setup, and processor choice. The refinements across the board make it a worthy upgrade, though, and a continuation of Google's excellent in the smartphone department. This is the phone to get when you want Google's vision for Android.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
